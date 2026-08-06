cfc8bd1b826c4881b033a2d15c18ff6158fc5946
kiransgithub <kiranjntukkd@gmail.com>
2026-08-05 18:05:03 -0700

    Add KCP PWA service worker

---
+++ web/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE = 'kcp-pilot-v1'
+const SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './config.js',
+  './manifest.webmanifest',
+  './icon-1024.png'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  const url = new URL(event.request.url)
+  if (url.origin !== self.location.origin) return
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone()
+        caches.open(CACHE).then(cache => cache.put(event.request, copy))
+        return response
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match('./index.html')))
+  )
+})
